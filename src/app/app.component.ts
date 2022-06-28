80fec4f85c784e1480561f1fa45a0d874ced2242
Carlos Angulo <carlosalbertoangulomendoza@gmail.com>
2022-06-28 17:21:51 -0500

    calendly window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'myfisio';
+  
+  menuOpen = false;
+  isCalendlyOpen = false;
+  
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ){}
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    if (this.menuOpen) {
+      this.renderer.addClass(this.document.body, 'no-scroll');
+    } else {
+      this.renderer.removeClass(this.document.body, 'no-scroll');
+    }
+  }
+
+  onCalendlyOpen() {
+    this.isCalendlyOpen = true;
+    this.renderer.addClass(this.document.body, 'no-scroll');
+  }
+
+  onCalendlyClose() {
+    this.isCalendlyOpen = false;
+    this.renderer.removeClass(this.document.body, 'no-scroll');
+  }
 }
