e3392ef94a3d9fff35cbdd66d6f817f0210666c6
Carlos Angulo <carlosalbertoangulomendoza@gmail.com>
2022-07-06 16:29:47 -0500

    anchor links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 100],
+};
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes, ExtraOptions } from '@angular/router';
-const routerOptions: ExtraOptions = {
-  anchorScrolling: "enabled",
-  scrollPositionRestoration: 'enabled'
-}
 
 @NgModule({
   declarations: [
@@ -16,7 +11,6 @@ const routerOptions: ExtraOptions = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    //RouterModule.forRoot(routes, routerOptions) 
   ],
   providers: [],
   bootstrap: [AppComponent]
